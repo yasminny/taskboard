1b60967dcac13b19e00fde2adb7e32a11841f40c
YASMIN NYGATE <YASMIN.NYGATE@GMAIL.COM>
2017-03-06 09:19:28 +0200

    jason board done

--- script/script.js
+++ script/script.js
@@ -3,10 +3,65 @@
  */
 let addListBtn = document.getElementById('list-btn');
 addListBtn.addEventListener('click', addList);
+const modal = document.querySelector('.modal');
+console.log(modal);
 
-const listTemplate = `<div class="list panel panel-default">
+function addCard(task, target) {
+  let ulElm = target.querySelector('.card-list');
+  let helper = document.createElement('div');
+
+  helper.innerHTML = `
+    <li class="card">
+      <button type="button" class="edit-card btn btn-info btn-xs" data-toggle="modal" data-target="#myModal">Edit card</button>
+      <p class="card-content">${task.text}</p>
+      <div class="member-list-on-card"></div>
+  </li>`;
+
+  const newCard = ulElm.appendChild(helper.querySelector('.card'));
+  const memberList = newCard.querySelector('div');
+  const members = task.members;
+
+  if (members.length > 0) {
+    for (let mem of members) {
+      const memberName = mem;
+      const nameArray = mem.split(' ');
+
+      const inital = nameArray[0].split('')[0] + nameArray[1].split('')[0];
+
+      memberList.innerHTML += `<span class="member-inital-on-card label label-primary" title="${memberName}">${inital}</span>`;
+    }
+  }
+
+  let editBtn = newCard.querySelector('.edit-card');
+  editBtn.addEventListener('click', editModalShow);
+}
+
+function editModalShow() {
+  modal.style.display = 'block';
+  modal.style.opacity = 1;
+const card = event.target.closest('.card');
+const list = card.closest('.list');
+
+const cardContent = card.querySelector('.card-content').innerHTML;
+console.log(cardContent);
+const editContent = modal.querySelector('textarea');
+editContent.innerHTML = cardContent;
+
+const listLists = modal.querySelector('.form-control');
+  const listHeader = list.querySelector('.panel-title').innerHTML;
+  const listHeaders = document.querySelectorAll('.panel-title');
+  for (const header of listHeaders){
+    let header = header.innerHTML;
+    console.log(header);
+    listLists.innerHTML = `<option>${header}</option>`;
+  }
+}
+
+function addList(list) {
+  let title = list.title || 'New List';
+  const listTemplate = `<div class="list panel panel-default">
       <div class="list-header panel-heading">
-        <h3 class="panel-title" >New List</h3>
+        <h3 class="panel-title" >${title}</h3>
         <input type="text" maxlength="30">
         <div class="dropdown">
           <button class="list-arrow btn btn-default dropdown-toggle" type="button" id="dropdownMenu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
@@ -24,38 +79,23 @@ const listTemplate = `<div class="list panel panel-default">
   <button class="card-btn add-card panel-footer">Add a card...</button>
 </div>`;
 
-const cardTemplate = `<li class="card"><button type="button" class="edit-card btn btn-info btn-xs">Edit card</button><p class="card-content"></p><div class="member-list-on-card">
-            </div></li>`;
-
-const memberInitalTemplate = `<span class="member-inital-on-card label label-primary"></span>`;
-
-function addCard() {
-  const parent = event.target.closest('.list');
-  console.log(parent);
-  let ulElm = parent.querySelector('.card-list');
-  ulElm.innerHTML += cardTemplate;
-}
-
-function addList() {
   addListBtn = document.getElementById('list-btn');
   let parent = addListBtn.closest('main');
   let helper = document.createElement('div');
   helper.innerHTML = listTemplate;
-  while (helper.firstChild) {
-    parent.insertBefore(helper.firstChild, addListBtn);
-  }
+  const newList = parent.insertBefore(helper.firstChild, addListBtn);
   initListTitles();
-  // addEventToAddCard(helper.firstChild);
+  return newList;
 }
 
 function titleClickHandler() {
 
-    const target = event.target;
-    target.style.display = 'none';
-    const inputElm = target.parentNode.querySelector('input');
-    inputElm.value = target.textContent;
-    inputElm.style.display = 'inline-block';
-    inputElm.focus();
+  const target = event.target;
+  target.style.display = 'none';
+  const inputElm = target.parentNode.querySelector('input');
+  inputElm.value = target.textContent;
+  inputElm.style.display = 'inline-block';
+  inputElm.focus();
 }
 
 function titleInputKeyHandler() {
@@ -92,55 +132,58 @@ function titleDeleteClickHandler() {
 function deleteListHandler() {
   const target = event.target;
   const titleElm = target.closest('.list-header').querySelector('h3');
-const listName = titleElm.innerHTML;
+  const listName = titleElm.innerHTML;
   let answer = confirm(`Deleting ${listName} list. Are you sure?`);
 
   const listElm = target.closest('.list');
   console.log(target.parentNode.parentNode.parentNode);
-  if (answer){
-listElm.parentNode.removeChild(listElm);
+  if (answer) {
+    listElm.parentNode.removeChild(listElm);
   } else {
-const editElm = listElm.querySelector('.dropdown-menu');
+    const editElm = listElm.querySelector('.dropdown-menu');
     editElm.style.display = 'none';
   }
   console.log(editElm);
 }
 
-// function editCardHandler() {
-//
-// }
 
-
-
-
-
-function xhrLoadHandler(){
+function xhrLoadHandler() {
   const myXhr = event.target;
   const contentType = myXhr.getResponseHeader('content-type');
 
   let data;
-  if (contentType.includes('json')){
+  if (contentType.includes('json')) {
     data = JSON.parse(myXhr.response);
   }
-  else{
+  else {
     data = myXhr.response;
   }
-  console.log(data);
+  const lists = data.board;
+
+  initListsFromData(lists);
 }
 
 const xhr = new XMLHttpRequest();
 xhr.addEventListener('load', xhrLoadHandler);
 
-xhr.open('GET', 'board.json');
+xhr.open('GET', 'assets/board.json');
 xhr.send();
 
 
 function initListTitles(targetList) {
   const targetParent = targetList || document;
 
+  //TODO: move inside creatio
+
   const addCardBtn = targetParent.getElementsByClassName('card-btn');
   for (const card of addCardBtn) {
-    card.addEventListener('click', addCard);
+    card.addEventListener('click', function (e) {
+      const emptyCard = {
+        "text": "",
+        "members": []
+      };
+      addCard(emptyCard, e.target.closest('.list'));
+    });
   }
 
   const titleElem = targetParent.querySelectorAll('.list-header h3');
@@ -164,11 +207,29 @@ function initListTitles(targetList) {
     EditBtn.addEventListener('click', deleteListHandler);
   }
 
-  // const EditCardElem = targetParent.querySelector('.edit-card');
-  // for (const EditCard of EditCardElem) {
-  //   EditBtn.addEventListener('click', editCardHandler);
-  // }
-
 }
 
 initListTitles();
+
+function initListsFromData(lists) {
+  let newList;
+  for (const list of lists) {
+    newList = addList(list);
+    if (list.tasks.length > 0) {
+      for (task of list.tasks) {
+        addCard(task, newList);
+      }
+    }
+  }
+}
+
+const xBtn = modal.querySelector('span');
+const closeEditBtn = modal.querySelector('.close-modal-btn');
+closeEditBtn.addEventListener('click', editModalHide);
+
+xBtn.addEventListener('click', editModalHide);
+
+function editModalHide() {
+  modal.style.display = 'none';
+  modal.style.opacity = 0;
+}
