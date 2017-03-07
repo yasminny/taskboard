508dd0e1fe3aa3c44b36df77db2ee5d67d27fa57
YASMIN NYGATE <YASMIN.NYGATE@GMAIL.COM>
2017-03-07 16:35:21 +0200

    members almost done, needs save edit changes

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 /**
  * Created by NEXUS on 26/02/2017.
  */
-
+// AJAX pull data from json and initialize lists, cards and members.
 function xhrLoadHandler() {
   const myXhr = event.target;
   const contentType = myXhr.getResponseHeader('content-type');
@@ -64,28 +64,42 @@ function initMembersFromData(members) {
   }
 }
 
+
+// initializes basic event listiners
 let addListBtn = document.getElementById('list-btn');
 addListBtn.addEventListener('click', addList);
 const modal = document.querySelector('.modal');
 
-const boardBtn = document.querySelector('.board-btn');
-const membersBtn = document.querySelector('.members-btn');
-boardBtn.addEventListener('click', changeMembersToBoard);
-membersBtn.addEventListener('click', changeBoardToMembers);
+function eventListinerToNav() {
+  const boardBtn = document.querySelector('.board-btn');
+  const membersBtn = document.querySelector('.members-btn');
+  boardBtn.addEventListener('click', changeMembersToBoard);
+  membersBtn.addEventListener('click', changeBoardToMembers);
+}
+
+eventListinerToNav();
 
 function changeBoardToMembers() {
+  const navBar = document.querySelector('.navbar-nav');
   const main = document.querySelector('main');
   const members = document.querySelector('#member-section');
   main.style.display = 'none';
   members.style.display = 'block';
-
+  navBar.innerHTML = `
+<li class="board-btn"><a href="#">Board </a></li>
+<li class="active members-btn"><a href="#">Members<span class="sr-only">(current)</span></a></li>`;
+  eventListinerToNav();
 }
 
 function changeMembersToBoard() {
+  const navBar = document.querySelector('.navbar-nav');
   const main = document.querySelector('main');
   const members = document.querySelector('#member-section');
   main.style.display = 'inline-flex';
   members.style.display = 'none';
+  navBar.innerHTML = `<li class="active board-btn"><a href="#">Board <span class="sr-only">(current)</span></a></li>
+          <li class="members-btn"><a href="#">Members</a></li>`;
+  eventListinerToNav();
 }
 
 
@@ -285,11 +299,70 @@ function addMember(member){
   const membersGroup = document.querySelector('.members-group');
   const addMemberSection = document.querySelector('.add-member');
   const helper = document.createElement('div');
-  member = member || event.target;
-  helper.innerHTML += `<li class="list-group-item">${member}</li>`;
+  helper.innerHTML += `<li class="list-group-item member-item"><h3 class="member-name">${member}</h3>
+<input type="text" class="edit-input-mem form-control">
+<div class="edit-member-btn">
+<button type="button" class="btn btn-default cancel-edit-mem">Cancel</button>
+<button type="button" class="btn btn-success save-edit-mem">Save</button>
+</div>
+<div class="member-btns">
+<button type="button" class="btn btn-info edit-member">Edit</button>
+<button type="button" class="btn btn-danger delete-member">Delete</button>
+</div></li>`;
   const newMember = membersGroup.insertBefore(helper.firstChild, addMemberSection);
+  newMember.querySelector('.delete-member').addEventListener('click', deleteMember);
+  newMember.querySelector('.edit-member').addEventListener('click', editMemberModeToggle);
+  newMember.querySelector('.cancel-edit-mem').addEventListener('click', editMemberModeToggle);
+  newMember.querySelector('.save-edit-mem').addEventListener('click', editMemberSave);
   return newMember;
 }
 
+function addMemberByUser() {
+  const membersGroup = document.querySelector('.members-group');
+  const addMemberSection = document.querySelector('.add-member');
+  const helper = document.createElement('div');
+  const newUser = document.querySelector('.add-member-input').value;
+  console.log(newUser);
+  if (newUser !== ''){
+    helper.innerHTML += `<li class="list-group-item member-item"><h3 class="member-name">${newUser}</h3>
+<input type="text" class="edit-input-mem form-control">
+<div class="edit-member-btn">
+<button type="button" class="btn btn-default cancel-edit-mem">Cancel</button>
+<button type="button" class="btn btn-success save-edit-mem">Save</button>
+</div>
+<div class="member-btns">
+<button type="button" class="btn btn-info edit-member">Edit</button>
+<button type="button" class="btn btn-danger delete-member">Delete</button>
+</div></li>`;
+    const newMember = membersGroup.insertBefore(helper.firstChild, addMemberSection);
+    document.querySelector('.add-member-input').value = '';
+    newMember.querySelector('.delete-member').addEventListener('click', deleteMember);
+    newMember.querySelector('.edit-member').addEventListener('click', editMemberModeToggle);
+    newMember.querySelector('.cancel-edit-mem').addEventListener('click', editMemberModeToggle);
+    newMember.querySelector('.save-edit-mem').addEventListener('click', editMemberSave);
+    return newMember;
+  }
+}
+
 const addMemberBtn = document.querySelector('.add-member-btn');
-addMemberBtn.addEventListener('click', addMember);
+addMemberBtn.addEventListener('click', addMemberByUser);
+
+function deleteMember() {
+  const target = event.target;
+  const liElm = target.closest('.member-item');
+  liElm.remove();
+}
+
+function editMemberModeToggle() {
+ const liElm = event.target.closest('.member-item');
+ liElm.classList.toggle('edit-mode');
+ const name = liElm.querySelector('.member-name').innerHTML;
+ const editInput = liElm.querySelector('.edit-input-mem');
+ editInput.value = name;
+}
+
+function editMemberSave() {
+  const liElm = event.target.closest('.member-item');
+  liElm.classList.toggle('edit-mode');
+  
+}
